a6ce5b451c07ef0cd5f6f7f37c6a2d0eabbab4c2
Abhra Basak <abhra@outlook.in>
2025-01-21 04:28:58 -0800

    Remap directories in config

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://abhra.in",
-  srcDir: "src",
-  outDir: "deploy",
+  srcDir: "source",
+  outDir: "webapp",
   compressHTML: false,
   vite: {
     build: {
