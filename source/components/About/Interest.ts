6cd6e90c408504489ed90ec7afe548f4c72f094e
Abhra Basak <abhra@outlook.in>
2025-02-18 19:06:45 -0800

    Migrate the last 2 data types completely to YAML

--- source/components/About/Interest.ts
+++ source/components/About/Interest.ts
@@ -1,6 +1,20 @@
+import { z } from "astro:content";
+
 export interface Interest {
   title: string;
   enabled: boolean;
   excerpt: string;
   icons: Record<string, { profile: string; icon: string }>;
 }
+
+export const zs = z.object({
+  title: z.string(),
+  enabled: z.boolean(),
+  excerpt: z.string(),
+  icons: z.record(z.object({
+    profile: z.string(),
+    icon: z.string(),
+  })),
+});
+
+export type zt = z.infer<typeof zs>;
