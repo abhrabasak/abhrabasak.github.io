b753b9fe812502f3a641b612575783ee5ef71265
Abhra Basak <abhra@outlook.in>
2025-02-20 19:06:35 -0800

    Use YAML data in work experience

--- source/components/Experience/Experience.ts
+++ source/components/Experience/Experience.ts
@@ -1,3 +1,5 @@
+import { z } from 'astro:content';
+
 export interface Experience {
   company: string;
   position: string;
@@ -6,3 +8,14 @@ export interface Experience {
   icon: string[];
   description: string;
 }
+
+export const zs = z.object({
+  company: z.string(),
+  position: z.string(),
+  project: z.string(),
+  date: z.string(),
+  icon: z.array(z.string()),
+  description: z.string(),
+});
+
+export type zt = z.infer<typeof zs>;
