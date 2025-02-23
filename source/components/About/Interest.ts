bb2969ab3feaba0407ae30e9bcfe063e3ae26183
Abhra Basak <abhra@outlook.in>
2025-02-22 18:02:55 -0800

    Interest Cards using Hypericons

--- source/components/About/Interest.ts
+++ source/components/About/Interest.ts
@@ -4,17 +4,14 @@ export interface Interest {
   title: string;
   enabled: boolean;
   excerpt: string;
-  icons: Record<string, { profile: string; icon: string }>;
+  icons: string[];
 }
 
 export const zs = z.object({
   title: z.string(),
   enabled: z.boolean(),
   excerpt: z.string(),
-  icons: z.record(z.object({
-    profile: z.string(),
-    icon: z.string(),
-  })),
+  icons: z.string().array(),
 });
 
 export type zt = z.infer<typeof zs>;
