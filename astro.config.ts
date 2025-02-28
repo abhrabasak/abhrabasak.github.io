c9354cada8c40249093e4a4aaf4742ae8f8d61eb
Abhra Basak <abhra@outlook.in>
2025-02-28 01:09:19 -0800

    Install Tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import yaml from '@rollup/plugin-yaml';
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   outDir: "webapp",
   compressHTML: false,
   vite: {
-    plugins: [yaml()],
+    plugins: [yaml(), tailwindcss()],
     build: {
       assetsInlineLimit: 4,
     },
