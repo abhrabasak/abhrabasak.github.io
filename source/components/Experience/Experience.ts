44589bde03ea4cefef3bcb52e58f32d511a2b86a
Abhra Basak <abhra@outlook.in>
2025-03-03 22:18:08 -0800

    Experience Page Styling and Content

--- source/components/Experience/Experience.ts
+++ source/components/Experience/Experience.ts
@@ -2,18 +2,22 @@ import { z } from 'astro:content';
 
 export interface Experience {
   company: string;
+  link: string;
   position: string;
   project: string;
   date: string;
+  location: string;
   icon: string[];
   description: string;
 }
 
 export const zs = z.object({
   company: z.string(),
+  link: z.string().url(),
   position: z.string(),
   project: z.string(),
   date: z.string(),
+  location: z.string(),
   icon: z.array(z.string()),
   description: z.string(),
 });
